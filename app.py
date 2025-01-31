c0ba1b00943635f20bf5c02826617d7d68526b46
Emerson <the.emerson.araujo@gmail.com>
2025-01-31 11:55:34 -0300

    adiciona suporte ao CORS na aplicação Flask

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
 db = SQLAlchemy(app)
+CORS(app)
 
 # Modelagem
     # Produto (id, name, price, description)
