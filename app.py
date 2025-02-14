35a2a08a6c7a1e6d1b95354fd85ae6754af7b0e0
Emerson <the.emerson.araujo@gmail.com>
2025-02-14 18:21:32 -0300

    adiciona gerenciador de login para autenticação de usuários

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask_login import UserMixin, login_user
+from flask_login import UserMixin, login_user, LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
+login_manager = LoginManager()
 db = SQLAlchemy(app)
 CORS(app)
 
